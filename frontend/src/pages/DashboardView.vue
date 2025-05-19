849b1f103d22cdb8e8b2c7807eea7be258ec4aca
Pazokita <amalia.maturana@gmail.com>
2025-05-19 18:30:50 +0200

    feat(dashboard): add dashboard view to display events

---
+++ frontend/src/pages/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import EventList from ',/components/EventList.vue'
+</script>
+
+<template>
+  <section class="p-6">
+    <h1 class="text-2xl font-bold mb-4">📆 Tableau de bord - Événements médicaux</h1>
+    <EventList />
+  </section>
+</template>
