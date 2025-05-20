0f270c19c2798a632e180b3701f3fedb632b42e8
Pazokita <amalia.maturana@gmail.com>
2025-05-20 17:47:03 +0200

    feat(router): ajout du router avec les routes principales de l'application

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
-
 <script setup lang="ts">
 import EventList from './components/EventList.vue';
 </script>
 
 <template>
-  <main class="min-h-screen bg-white text-gray-900 p-6">
-    <h1 class="text-2xl font-bold mb-4">UBAQ Compliance App</h1>
-    <EventList />
-  </main>
+  <router-view />
 </template>
 
-<style scoped>
+<style>
+body {
+  font-family: 'Inter', sans-serif;
+  @apply bg-[#FDFBF9];
+}
 </style>
