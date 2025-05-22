0a3cd25dcb0f0647c52e5e25b968a6dd6f93a4ad
Pazokita <amalia.maturana@gmail.com>
2025-05-22 15:10:02 +0200

    feat(apollo): injecte Apollo Client globalement via provide() dans main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { apolloClient } from "./lib/apollo";
 
-const app = createApp(App)
-app.use(router) 
-app.mount('#app')
+const app = createApp({
+    setup() {
+      provide(DefaultApolloClient, apolloClient)
+      return () => h(App)
+    }
+  })
+  
+  app.use(router)
+  app.mount('#app')
